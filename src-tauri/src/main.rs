fa939e7362df0936d3bae3cf33c33c95a2f01b8d
se2crid <151872490+se2crid@users.noreply.github.com>
2025-11-07 22:32:51 +0100

    Disables explicit sync on Linux if unset

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,16 +5,8 @@ fn main() {
     #[cfg(target_os = "linux")]
     {
         use std::env;
-        if env::var_os("WEBKIT_DISABLE_DMABUF_RENDERER").is_none() {
-            env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
-        }
-
-        if env::var_os("GDK_BACKEND").is_none() {
-            if env::var_os("WAYLAND_DISPLAY").is_some() {
-                env::set_var("GDK_BACKEND", "wayland");
-            } else if env::var_os("DISPLAY").is_some() {
-                env::set_var("GDK_BACKEND", "x11");
-            }
+        if env::var_os("__NV_DISABLE_EXPLICIT_SYNC").is_none() {
+            env::set_var("__NV_DISABLE_EXPLICIT_SYNC", "1");
         }
     }
 
